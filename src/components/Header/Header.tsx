995d5e788bc1c8123703227a4760ce2cb9215b8d
Bernard Hechter <bernardh0602@gmail.com>
2023-04-27 19:49:09 +0200

    added a counter animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,8 @@ import {memo} from "react"
 
 import AdvancedLogo from "/images/logo-bonus.svg";
 import NormalLogo from "/images/logo.svg";
-import classes from "./Header.module.css"
+import classes from "./Header.module.css";
+import {motion} from "framer-motion"
 interface HeaderProps{
   gameModeNormal:boolean;
     score:number
@@ -18,7 +19,7 @@ const Header = ({gameModeNormal,score}:HeaderProps) => {
         <h1 aria-label="Rock Paper Scissors Game"><img className={classes["header-logo"]} src={currentLogo} alt="Logo" /></h1>
         <div className={classes["score-container"]}>
             <h2 className={classes["score-heading"]}>Score</h2>
-            <span className={classes["score-number"]}>{score}</span>
+            <motion.span key={score} animate={{scale:1}} initial={{scale:0}}  className={classes["score-number"]}>{score}</motion.span>
         </div>
     </header>
   )
