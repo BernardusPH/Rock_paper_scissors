54b22a2c8a7d321dfe46d2f7a08874237a84e3b3
Bernard Hechter <bernardh0602@gmail.com>
2023-04-27 18:05:32 +0200

    simple mode shape swap

--- src/game_Img_storage/simpleGameStorage.ts
+++ src/game_Img_storage/simpleGameStorage.ts
@@ -1,12 +1,7 @@
 import { ImgData } from "./advancedGameStorage.ts";
 
 export const simpleGameImgObj: ImgData[] = [
-  {
-    img: "/Rock_paper_scissors/images/icon-scissors.svg",
-    name: "scissors",
-    bgcolorStart: "39, 89%, 49%",
-    bgcolorEnd: "40, 84%, 53%",
-  },
+ 
   {
     img: "/Rock_paper_scissors/images/icon-paper.svg",
     name: "paper",
@@ -14,9 +9,15 @@ export const simpleGameImgObj: ImgData[] = [
     bgcolorEnd: "230, 89%, 65%",
   },
   {
+    img: "/Rock_paper_scissors/images/icon-scissors.svg",
+    name: "scissors",
+    bgcolorStart: "39, 89%, 49%",
+    bgcolorEnd: "40, 84%, 53%",
+  },
+  {
     img: "/Rock_paper_scissors/images/icon-rock.svg",
     name: "rock",
     bgcolorStart: "349, 71%, 52%",
     bgcolorEnd: "349, 70%, 56%",
-  },
+  }
 ];
