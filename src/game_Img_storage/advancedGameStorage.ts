2fdc72fe510bb1a57bf8f7c7db4ef6ab9265940f
Bernard Hechter <bernardh0602@gmail.com>
2023-04-27 17:15:45 +0200

    image paths fixed

--- src/game_Img_storage/advancedGameStorage.ts
+++ src/game_Img_storage/advancedGameStorage.ts
@@ -7,34 +7,35 @@ export interface ImgData {
 
 // - Lizard Gradient: hsl(261, 73%, 60%) to hsl(261, 72%, 63%)
 // - Cyan: hsl(189, 59%, 53%) to hsl(189, 58%, 57%)
+
 export const advancedGameImgObj: ImgData[] = [
   {
-    img: "/images/icon-scissors.svg",
+    img: "../../public/images/icon-scissors.svg",
     name: "scissors",
     bgcolorStart: "39, 89%, 49%",
     bgcolorEnd: "40, 84%, 53%",
   },
   {
-    img: "/images/icon-paper.svg",
+    img: "../../public/images/icon-paper.svg",
     name: "paper",
     bgcolorStart: "230, 89%, 62%",
     bgcolorEnd: "230, 89%, 65%",
   },
 
   {
-    img: "/images/icon-rock.svg",
+    img: "../../public/images/icon-rock.svg",
     name: "rock",
     bgcolorStart: "349, 71%, 52%",
     bgcolorEnd: "349, 70%, 56%",
   },
   {
-    img: "/images/icon-lizard.svg",
+    img: "../../public/images/icon-lizard.svg",
     name: "lizard",
     bgcolorStart: "261, 73%, 60%",
     bgcolorEnd: "261, 72%, 63%",
   },
   {
-    img: "/images/icon-spock.svg",
+    img: "../../public/images/icon-spock.svg",
     name: "spock",
     bgcolorStart: "189, 59%, 53%",
     bgcolorEnd: "189, 58%, 57%",
--- src/game_Img_storage/simpleGameStorage.ts
+++ src/game_Img_storage/simpleGameStorage.ts
@@ -2,19 +2,19 @@ import { ImgData } from "./advancedGameStorage.ts";
 
 export const simpleGameImgObj: ImgData[] = [
   {
-    img: "/images/icon-scissors.svg",
+    img: "../../public/images/icon-scissors.svg",
     name: "scissors",
     bgcolorStart: "39, 89%, 49%",
     bgcolorEnd: "40, 84%, 53%",
   },
   {
-    img: "/images/icon-paper.svg",
+    img: "../../public/images/icon-paper.svg",
     name: "paper",
     bgcolorStart: "230, 89%, 62%",
     bgcolorEnd: "230, 89%, 65%",
   },
   {
-    img: "/images/icon-rock.svg",
+    img: "../../public/images/icon-rock.svg",
     name: "rock",
     bgcolorStart: "349, 71%, 52%",
     bgcolorEnd: "349, 70%, 56%",
